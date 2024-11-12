6df722de4c58dbe300023b71da0cf8c3a47b9e29
Ana Ramos Moya <anaana609@gmail.com>
2024-11-12 21:14:14 +0100

    added checkline and divideline functions

--- get_next_line2.c
+++ get_next_line2.c
@@ -6,49 +6,77 @@
 /*   By: aramos-m <aramos-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 20:09:50 by aramos-m          #+#    #+#             */
-/*   Updated: 2024/11/10 20:38:05 by aramos-m         ###   ########.fr       */
+/*   Updated: 2024/11/12 21:12:32 by aramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char    *resize_buffer(char *buffer, size_t bytesread)
 {
-    char    *result;
-    result = malloc(bytesread + 1);
-    if (!result)
-        return (NULL);
+	char    *result;
+	result = malloc(bytesread + 1);
+	if (!result)
+		return (NULL);
 	memmove(result, buffer, bytesread);
-    return (result);
+	return (result);
 }
 
-//Crear la función que separe las frases
-
-
-
-char    *get_next_line(int fd)
+char	*divide_line(char *buffer, int pos) //Función para extraer frases
 {
-    char    *buffer;
-    char    *result;
-    int     i;
-    
-    if (BUFFER_SIZE <= 0)
+	char    *readedline;
+	
+	readedline = malloc(i + 1); //Extraemos la línea y lo almacenamos en readedline
+	if (!readedline)
 		return (NULL);
-    buffer = malloc(BUFFER_SIZE + 1);
-    if (!buffer)
-        return (NULL);
-    bytesread = read(fd, buffer, BUFFER_SIZE);
-    
-    i = 0;
-    while (i < bytesread)
-    { 
-        if (buffer[i] == '\n')
-        {
-            //Llamar función para separar las frases.
-            return (); // Devolver lo que retorne la función de separar frases.
-        }
-		else if (i == bytesread)
+	memmove(readedline, buffer, i);
+	if (i != bytesread)
+	{
+		nextread = malloc(bytesread - i); //Guardamos el resto de la lectura en nextread
+		memmove(nextread, &buffer[i + 1], bytesread - i);
+	}
+	return (readedline);
+}
+
+char	*check_line(char **nextread, char *buffer, ssize_t bytesread)
+{
+	int	i;
+	
+	i = 0;
+	while (i < bytesread)
+	{
+		if (buffer[i] == '\n')
+			return (divide_line(buffer, i, &nextread)); // Devolver lo que retorne la función de separar frases
+		i++;
+		if (i == bytesread)
 		{
-            buffer = resize_buffer(buffer, bytesread);
+			buffer = resize_buffer(buffer, bytesread);
 			bytesread += read(fd, &buffer[i], BUFFER_SIZE); //Pasa el punto de la lectura en el que se quedó.
+			buffer[bytesread] = '\0';
 		}
-    }
+	}
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*nextread;
+	ssize_t		bytesread;
+	
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!nextread)
+	{
+		buffer = malloc(BUFFER_SIZE + 1);
+		if (!buffer)
+			return (NULL);
+		bytesread = read(fd, buffer, BUFFER_SIZE);
+		buffer[bytesread] = '\0';
+	}
+	else
+	{
+		buffer = nextread;
+		bytesread = ft_strlen(buffer);
+		nextread = 0;
+	}
+	return (check_line(&nextread, buffer, bytesread));
 }
