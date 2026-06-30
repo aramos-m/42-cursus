ea36018fb6277ee8b8552f2976da57e319f532a4
aramos-m <anaana609@gmail.com>
2026-06-30 23:10:14 +0200

    fix: parser accepts sign in argv "+5"

--- 03-philosophers/src/init.c
+++ 03-philosophers/src/init.c
@@ -15,16 +15,24 @@
 static int	ft_atoi(const char *str)
 {
 	int		i;
+	int		sign;
 	long	n;
 
 	i = 0;
+	sign = 1;
 	n = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		n = n * 10 + (str[i] - '0');
 		i++;
 	}
-	return ((int)n);
+	return ((int)(n * sign));
 }
 
 static int	parser(int argc, char **argv)
@@ -36,7 +44,9 @@ static int	parser(int argc, char **argv)
 	while (i < argc)
 	{
 		j = 0;
-		if (argv[i][0] == '\0')
+		if (argv[i][j] == '+' || argv[i][j] == '-')
+			j++;
+		if (argv[i][j] == '\0')
 			return (0);
 		while (argv[i][j])
 		{
